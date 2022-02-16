5c2724c44acc34fb73e04b7b4eb09f2f37c752e1
raquelMoya <rakelms90@gmail.com>
2022-02-16 13:34:53 +0100

    Creados modulos y migraciones de User y Movie

---
+++ migrations/20220216121339-create-movie.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Movies', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      sinopsis: {
+        type: Sequelize.STRING
+      },
+      adult: {
+        type: Sequelize.BOOLEAN
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      fecha: {
+        type: Sequelize.STRING
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Movies');
+  }
+};
